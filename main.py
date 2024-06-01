591d8400c9628682e5ebb43a242efcd1e45f2822
realman2k <53941338+realman2k@users.noreply.github.com>
2024-06-01 11:49:48 +0300

    Выполнена Задача 2

--- main.py
+++ main.py
@@ -26,27 +26,72 @@ class Teacher:
         self.__experience = experience
 
     def get_teacher_data(self):
-        return f'{self.__name}, образование {self.__education}, опыт работы {self.__experience} года.'
+        return f'{self.get_name()}, образование {self.get_education()}, опыт работы {self.get_experience()} года.'
 
     def add_mark(self, student_name, mark):
-        return f'{self.__name} поставил оценку {mark} студенту {student_name}.'
+        return f'{self.get_name()} поставил оценку {mark} студенту {student_name}.'
 
     def remove_mark(self, student_name, mark):
-        return f'{self.__name} удалил оценку {mark} студенту {student_name}.'
+        return f'{self.get_name()} удалил оценку {mark} студенту {student_name}.'
 
     def give_a_consultation(self, class_name):
-        return f'{self.__name} провел консультацию в классе {class_name}.'
+        return f'{self.get_name()} провел консультацию в классе {class_name}.'
 
 
 teacher_1 = Teacher('Иван Петров', 'БГПУ', 4)
 print(teacher_1.get_teacher_data())
 print(teacher_1.add_mark('Петр Сидоров', 4))
-print(teacher_1.remove_mark('Петр Сидоровв', 3))
+print(teacher_1.remove_mark('Петр Сидоров', 3))
 print(teacher_1.give_a_consultation('9Б'))
+print()
+
 
 # Задача 2
 # Написать класс наследник DisciplineTeacher, его классом родителем (базовым классом) будет класс Teacher, ему
 # необходимо добавить 2 новых атрибута. discipline - предмет его надо перенести из класса Teacher; job_title - должность
 # (например завуч, директор, учитель старших классов). Все атрибуты необходимо сделать защищенными. Для данных атрибутов
 # необходимо написать геттеры (для всех) а для атрибута job_title также необходим еще и сеттер. Далее необходимо
-# адаптировать методы класса родителя а именно: get_teacher_data add_mark remove_mark give_a_consultation
+# адаптировать методы класса родителя, а именно: get_teacher_data add_mark remove_mark give_a_consultation
+
+class DisciplineTeacher(Teacher):
+    def __init__(self, name, education, experience, discipline, job_title):
+        super().__init__(name, education, experience)
+        self.__discipline = discipline
+        self.__job_title = job_title
+
+    def get_discipline(self):
+        return self.__discipline
+
+    def get_job_title(self):
+        return self.__job_title
+
+    def set_job_title(self, job_title):
+        self.__job_title = job_title
+        return self.__job_title
+
+    def get_teacher_data(self):
+        return (f'{self.get_name()}, образование {self.get_education()}, опыт работы {self.get_experience()} года.\n'
+                f'Предмет {self.get_discipline()}, должность {self.get_job_title()}')
+
+    def add_mark(self, student_name, mark, discipline):
+        return (f'{self.get_name()} поставил оценку {mark} студенту {student_name}.\n'
+                f'Предмет: {self.get_discipline()}')
+
+    def remove_mark(self, student_name, mark):
+        return (f'{self.get_name()} удалил оценку {mark} студенту {student_name}.\n'
+                f'Предмет: {self.get_discipline()}')
+
+    def give_a_consultation(self, class_name):
+        return (f'{self.get_name()} провел консультацию в классе {class_name}.\n'
+                f'По предмету {self.get_discipline()} как {self.get_job_title()}')
+
+
+discipline_teacher = DisciplineTeacher('Иван Петров', 'БГПУ', 4, 'Химия',
+                                       'Директор')
+print(discipline_teacher.get_teacher_data())
+print()
+print(discipline_teacher.add_mark('Николай Иванов', 4, 'Химия'))
+print()
+print(discipline_teacher.remove_mark('Дмитрий Сидоров', 3))
+print()
+print(discipline_teacher.give_a_consultation('10Б'))
